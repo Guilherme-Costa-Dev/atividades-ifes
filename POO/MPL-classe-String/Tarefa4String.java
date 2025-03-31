bd790b1497d2c766175fbd45bd2a0368f2a6e93b
Guilherme Cordeiro Costa <guilhermecosta030409@gmail.com>
2025-03-31 18:44:48 -0300

    asf

--- POO/MPL-classe-String/Tarefa4String.java
+++ POO/MPL-classe-String/Tarefa4String.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Tarefa4String {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -8,48 +9,46 @@ public class Tarefa4String {
         scanner.close();
     }
 
-    public static void genLosango(int num){
+    public static void genLosango(int num) {
         String abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String los = "";
         num--;
 
-        for (int i = 0 ; i<=num/2 ; i++) {
-            for (int j = num ; j>i ; j--) {
+        for (int i = 0; i <= num / 2; i++) {
+            for (int j = num; j > i; j--) {
                 los += " ";
             }
             if (i == 0) {
                 for (int j = 0; j <= i; j++) {
                     los += abc.charAt(j);
                 }
-            }
-            else{
-                for (int j = 0 ; j <= i*2 ; j++) {
+            } else {
+                for (int j = 0; j <= i * 2; j++) {
                     los += abc.charAt(j);
                 }
             }
-            los = los.substring(num/2);
+            los = los.substring(num / 2);
             System.out.println(los);
             los = "";
         }
 
-        for (int i = 1 ; i<=num/2 ; i++) {
-            for (int j = 0 ; j<num/2 ; j++) {
+        for (int i = 1; i <= num / 2; i++) {
+            for (int j = 0; j < num / 2; j++) {
                 los += " ";
             }
-            for (int j = 0 ; j<i ; j++) {
+            for (int j = 0; j < i; j++) {
                 los += " ";
             }
             if (i == num) {
                 for (int j = num; j <= i; j--) {
                     los += abc.charAt(j);
                 }
-            }
-            else{
-                for (int j = num ; j >= i*2 ; j--) {
+            } else {
+                for (int j = num; j >= i * 2; j--) {
                     los += abc.charAt(j);
                 }
             }
-            los = los.substring(num/2);
+            los = los.substring(num / 2);
             System.out.println(los);
             los = "";
         }
