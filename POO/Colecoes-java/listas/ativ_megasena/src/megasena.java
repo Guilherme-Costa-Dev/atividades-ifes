d8e5b03292ba1c0c0a6d42634a9626acbebb0dc8
Guilherme Cordeiro Costa <guilhermecosta030409@gmail.com>
2025-05-29 16:04:08 -0300

    avaliativa2 50%

--- POO/Colecoes-java/listas/ativ_megasena/src/megasena.java
+++ POO/Colecoes-java/listas/ativ_megasena/src/megasena.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class megasena {
     public static void main(String[] args) {
---
+++ POO/Colecoes-java/listas/atividade_avaliativa2/src/Automovel.java
@@ -0,0 +1,28 @@
+public class Automovel {
+    String placa;
+    String modelo;
+    int quantidade_de_passageiros;
+    double peso_fabrica;
+    int quantidade_litros_gasolina;
+
+    public Automovel (String placa, String modelo, int quantidade_de_passageiros, double peso_fabrica, int quantidade_litros_gasolina) {
+        this.modelo = modelo;
+        this.placa = placa;
+        this.peso_fabrica = peso_fabrica;
+        this.quantidade_de_passageiros = quantidade_de_passageiros;
+        this.quantidade_litros_gasolina = quantidade_litros_gasolina;
+    }
+
+    public double pesoTotal(double peso_fabrica, int quantidade_de_passageiros, int quantidade_litros_gasolina){
+        return peso_fabrica+(quantidade_de_passageiros*5)+(quantidade_litros_gasolina*0.75);
+    }
+
+    public String toString(){
+        return "Placa: "+placa+"\n"+
+                "Modelo: "+modelo+"\n"+
+                "Quantidade de passageiros: "+quantidade_de_passageiros+"\n"+
+                "Quantidade de litros de gasolina: "+quantidade_litros_gasolina+"\n"+
+                "Peso de fabrica: "+peso_fabrica+"\n"+
+                "Peso total: "+pesoTotal(peso_fabrica, quantidade_de_passageiros, quantidade_litros_gasolina);
+    }
+}
