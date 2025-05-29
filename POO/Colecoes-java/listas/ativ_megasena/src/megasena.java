21786ee03896b98096d876f3ebd50ebb1ff8c360
Guilherme Cordeiro Costa <guilhermecosta030409@gmail.com>
2025-05-29 08:37:37 -0300

    megasena

---
+++ POO/Colecoes-java/listas/ativ_megasena/src/megasena.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class megasena {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+            for (int i = 0 ; i<2709 ; i++) {
+                String linha = scanner.nextLine();
+                String[] lista = linha.split("\t");
+
+                System.out.println(lista[0] + " " + lista[1] + " " +
+                                    lista[2] + " " + lista[3] + " " +
+                                    lista[4] + " " + lista[5]
+                );
+            }
+
+        scanner.close();
+    }
+}
