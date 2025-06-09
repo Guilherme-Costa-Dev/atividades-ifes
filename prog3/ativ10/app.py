780cfe8bcd4bace609a260c2bf6586f1222c6ac2
Guilherme Cordeiro Costa <guilhermecosta030409@gmail.com>
2025-06-09 18:46:17 -0300

    10 certa e 11 errada alessandra

--- prog3/ativ10/app.py
+++ prog3/ativ10/app.py
@@ -1,9 +1,10 @@
 import mod
+import sys
 def main():
 
-    nome = str(input())
-    nome2 = str(input())
-    lista1 = mod.lerArquivo(nome)
+    nome1 = sys.argv[1]
+    nome2 = sys.argv[2]
+    lista1 = mod.lerArquivo(nome1)
     lista2 = mod.lerArquivo(nome2)
     print(lista1)
     print(lista2)
