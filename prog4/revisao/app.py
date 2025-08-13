b776fb3a25f60be51c5e1a810531321f2b3b0b38
Guilherme Cordeiro Costa <guilhermecosta030409@gmail.com>
2025-08-13 18:07:31 -0300

    revisao

--- prog4/revisao/app.py
+++ prog4/revisao/app.py
@@ -3,11 +3,30 @@ import sys
 
 def main():
 
-    frase1 = sys.argv[0]
-    frase2 = sys.argv[1]
+    frase1 = sys.argv[1]
+    frase2 = sys.argv[2]
 
     lista1 = mod.remover_rep(frase1)
     lista2 = mod.remover_rep(frase2)
+    lista3 = mod.uniao(lista1, lista2)
+    lista4 = mod.intersecao(lista1, lista2)
+
+    lista5 = []
+    lista5.append(sorted(lista1))
+    lista5.append(sorted(lista2))
+    lista5.append(sorted(lista3))
+    lista5.append(sorted(lista4))
+    lista5.sort(key=len)
+
+    lista6 = []
+    for i in range(len(lista5)):
+        sublista = []
+        for j in lista5[i]:
+            sublista.append(j.upper())
+        lista6.append(sublista)
+    
+    for i in lista6:
+        print(i)
 
 if __name__ == "__main__":
     main()
--- prog4/revisao/mod.py
+++ prog4/revisao/mod.py
@@ -1,22 +1,21 @@
 def remover_rep(frase:str)->list:
     lista = list(frase)
-
     lista2 = []
-
     for i in lista:
-        repetido = False
-        for j in lista2:
-            if i == j:
-                repetido = True
-                break
-        if repetido == False:
+        if i not in lista2:
             lista2.append(i)
-
     return lista2
 
 def uniao(lista1:list, lista2:list)->list:
-    lista3 = lista1
+    lista3 = lista1.copy()
     for i in lista2:
-        if i in lista3:
+        if i not in lista3:
+            lista3.append(i)
+    return lista3
+
+def intersecao(lista1:list, lista2:list)->list:
+    lista3 = []
+    for i in lista1:
+        if i in lista2:
             lista3.append(i)
     return lista3
