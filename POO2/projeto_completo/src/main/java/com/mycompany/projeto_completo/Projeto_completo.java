6e32c06c444f59444e318c51d9b5b0aabd2a7a76
Guilherme Costa <guilhermecosta030409@gmail.com>
2025-11-18 16:47:32 -0300

    projeto poo

---
+++ POO2/projeto_completo/src/main/java/com/mycompany/projeto_completo/Projeto_completo.java
@@ -0,0 +1,12 @@
+package com.mycompany.projeto_completo;
+
+/**
+ *
+ * @author guilh
+ */
+public class Projeto_completo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
