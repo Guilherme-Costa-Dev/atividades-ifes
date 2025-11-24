32ef708f161926ddca3d6ea8a98f353393f6f90a
Guilherme Costa <guilhermecosta030409@gmail.com>
2025-11-24 17:52:03 -0300

    projeto gfinalizado

--- POO2/bd_projeto_completo/bd_projeto_completo.sql
+++ POO2/bd_projeto_completo/bd_projeto_completo.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE projetoPOO;
+gitCREATE DATABASE projetoPOO;
 
 USE projetoPOO;
 
@@ -17,6 +17,26 @@ CREATE TABLE Cliente(
     nomeCliente VARCHAR(30) NOT NULL,
     emailCliente VARCHAR(50) NOT NULL,
     PRIMARY KEY (idCliente)
+);
+
+CREATE TABLE Venda(
+	idVenda int NOT NULL auto_increment,
+    dataVenda date not null,
+    valorVenda float not null,
+    idCliente int not null,
+    primary key (idVenda)
+);
+
+create table ItemVenda(
+	idItemVenda int not null auto_increment,
+    idVenda int not null,
+    idProduto int not null,
+    qtdProduto int not null,
+    vlrUnitario float not null,
+    
+    primary key (idItemVenda),
+    foreign key (idVenda) references Venda(idVenda),
+    foreign key (idProduto) references Produto(idProduto)
 );
 
 SELECT * FROM cliente;
