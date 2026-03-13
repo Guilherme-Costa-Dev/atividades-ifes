b68fe181434ce23c8efd10f8333647709f8ae29e
Guilherme Cordeiro Costa <20241TIIMI0054@cefetes.br>
2026-03-13 08:46:00 -0300

    botao no celular vm

--- INFO5/mobile/Capitulo4a/app/src/main/java/com/example/capitulo4a/MainActivity.kt
+++ INFO5/mobile/Capitulo4a/app/src/main/java/com/example/capitulo4a/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.example.capitulo4a
 
+import android.app.Activity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.capitulo4a.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            Toast.makeText(this, "Turma do 5 módulo", Toast.LENGTH_SHORT).show()
         }
     }
+
+
+
 }
