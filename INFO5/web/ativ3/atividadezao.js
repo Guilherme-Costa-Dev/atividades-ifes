a5f2c83e806821c321c7696c295587309402dbec
Guilherme Cordeiro Costa <20241TIIMI0054@cefetes.br>
2026-03-16 12:45:33 -0300

    alterar

--- INFO5/web/ativ3/atividadezao.js
+++ INFO5/web/ativ3/atividadezao.js
@@ -77,5 +77,40 @@ function apagarDados(){
         alert("Apagado com sucesso")
     }
 
+    listar()
+
+}
+
+function alterar(){
+    let nomePesquisa = document.getElementById("pesquisa").value;
+    let novoNome = document.getElementById("nome").value;
+    let novoTel = document.getElementById("tel").value;
+    let novoEmail = document.getElementById("email").value;
+    let novoEndereco = document.getElementById("endereco").value;
+
+    if (nomePesquisa == "") {
+        alert("Digite o nome no campo de pesquisa para alterar");
+        return;
+    }
+
+    let deuCerto = 0;
+
+    for (let i = 0; i < lista.length; i += 4) {
+        if (nomePesquisa == lista[i]) {
+            lista[i] = novoNome || lista[i];
+            lista[i+1] = novoEmail || lista[i+1];
+            lista[i+2] = novoEndereco || lista[i+2];
+            lista[i+3] = novoTel || lista[i+3];
+            deuCerto = 1;
+            break;
+        }
+    }
+
+    if (deuCerto != 1) {
+        alert("Nome não encontrado");
+    } else {
+        alert("Alterado com sucesso!");
+        listar();
+    }
 }
 
