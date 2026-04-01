4d886251f80e568c364e3849d61a16708aea5095
¨Guilherme¨ <guilhermecosta030409@gmail.com>
2026-03-31 21:05:08 -0300

    ativi php finalizada

--- INFO5/web/php/ativ1/index.php
+++ INFO5/web/php/ativ1/index.php
@@ -2,20 +2,20 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Verificar Valor</title>
+    <title>Atividade 1 php</title>
 </head>
 <body>
 
 <h2>1 - Digite um valor:</h2>
 
 <form method="post">
-    <input type="number" name="valor">
+    <input type="number" name="valor1">
     <button type="submit">Enviar</button>
     <br><br>
 </form>
 
 <?php
-    $valor = $_POST["valor"];
+    $valor = $_POST["valor1"];
 
     if ($valor > 0) {
         echo "Valor Positivo";
@@ -29,19 +29,19 @@
 <h2>2 - Digite os valores: </h2>
 
 <form method="post">
-    <input type="number" name="a">
-    <input type="number" name="b">
-    <input type="number" name="c">
-    <input type="number" name="d">
+    <input type="number" name="a2">
+    <input type="number" name="b2">
+    <input type="number" name="c2">
+    <input type="number" name="d2">
     <button type="submit">Enviar</button>
 </form>
 <br>
 
 <?php
-    $a = $_POST["a"];
-    $b = $_POST["b"];
-    $c = $_POST["c"];
-    $d = $_POST["d"];
+    $a = $_POST["a2"];
+    $b = $_POST["b2"];
+    $c = $_POST["c2"];
+    $d = $_POST["d2"];
 
     $soma = $a + $c;
     $mult = $b * $d;
@@ -58,15 +58,15 @@
 <h2>3 - Digite A e B: </h2>
 
 <form method="post">
-    <input type="number" name="a">
-    <input type="number" name="b">
+    <input type="number" name="a3">
+    <input type="number" name="b3">
     <button type="submit">Enviar</button>
 </form>
 <br>
 
 <?php
-    $a = $_POST["a"];
-    $b = $_POST["b"];
+    $a = $_POST["a3"];
+    $b = $_POST["b3"];
 
     if ($a > $b) {
         echo $b, $a;
@@ -98,23 +98,58 @@
 <h2>5 - Digite 2 valores: </h2>
 
 <form method="post">
-    <input type="number" name="a">
-    <input type="number" name="b">
+    <input type="number" name="a5">
+    <input type="number" name="b5">
     <button type="submit">Enviar</button>
 </form>
 
 <?php
-    $a = $_POST["a"];
-    $b = $_POST["b"];
+    $a = $_POST["a5"];
+    $b = $_POST["b5"];
 
-    if ($a < $b) {
+    if ($a <= $b) {
         for ($i = $a; $i <= $b; $i++) {
-            echo $i;
+            echo "$i ";
         }
     } elseif ($a > $b) {
         echo "Primeiro numero é maior que o segundo";
     }
 ?>
 
+<h2>6 - Digite 2 valores: </h2>
+
+<form method="post">
+    <input type="number" name="a6">
+    <input type="number" name="b6">
+    <button type="submit">Enviar</button>
+</form> 
+
+<?php
+    $a = $_POST["a6"];
+    $b = $_POST["b6"];
+
+    $listaPar = [];
+    $listaImpar = [];
+
+    if ($a <= $b) {
+        for ($i = $a; $i <= $b; $i++) {
+            if ($i%2 == 0) {
+                $listaPar[] = $i;
+            } else {
+                $listaImpar[] = $i;
+            }
+        }
+        echo "Par: ";
+        foreach($listaPar as $sim) {
+            echo "$sim, ";
+        }
+        echo "Impar: ";
+        foreach($listaImpar as $sim) {
+            echo "$sim, ";
+        }
+    } else{
+        echo "Primeiro numero é maior que o segundo";
+    }
+?>
 </body>
 </html>
