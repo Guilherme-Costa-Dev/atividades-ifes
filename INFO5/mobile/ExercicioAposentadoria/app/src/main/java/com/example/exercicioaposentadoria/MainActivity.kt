e285d052b7dab113768fd549984b8c82cfef011c
Guilherme Cordeiro Costa <20241TIIMI0054@cefetes.br>
2026-04-23 12:29:15 -0300

    aposentadoria comeco

---
+++ INFO5/mobile/ExercicioAposentadoria/app/src/main/java/com/example/exercicioaposentadoria/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.exercicioaposentadoria
+
+import android.app.Activity
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+}
