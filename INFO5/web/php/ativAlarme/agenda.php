2eafa972b0fac560d0f83ca7cbc0618aa2c408e8
¨Guilherme¨ <guilhermecosta030409@gmail.com>
2026-05-26 20:09:04 -0300

    atividade agenda

---
+++ INFO5/web/php/ativAlarme/agenda.php
@@ -0,0 +1,63 @@
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Agenda Simples</title>
+</head>
+<body>
+    <h2>Relógio:</h2>
+    <h1 id="relogio"></h1>
+
+    <h3>Adicionar Compromisso</h3>
+    <form id="formCompromisso">
+        Horário (HH:MM): <input type="text" id="hora" required><br><br>
+        Assunto: <input type="text" id="assunto" required><br><br>
+        <button type="button" onclick="incluirComp()" >Gravar compromisso</button>
+    </form>
+
+    <script>
+        // Cria o vetor dos compromissos salvos
+        var compromissos = [];
+
+        // Atualiza o relógio
+        function atualizarRelogio() {
+            // Pega a hora atual e formata
+            var agora = new Date();
+            var horas = String(agora.getHours()).padStart(2, '0');
+            var minutos = String(agora.getMinutes()).padStart(2, '0');
+            var segundos = String(agora.getSeconds()).padStart(2, '0');
+            relogio = horas + ":" + minutos + ":" + segundos;
+
+            document.getElementById("relogio").innerHTML = relogio; // Coloca a hora atual no html
+            
+            horaAtual = horas + ":" + minutos; // Formata a hora para comparar com a hora do compromisso
+            
+            // Verifica se está na hora do compromisso
+            for (var i = 0; i < compromissos.length; i++) {
+                if (compromissos[i].hora == horaAtual) {
+                    alert("Lembrete: " + compromissos[i].assunto + " às " + compromissos[i].hora);
+                    window.location.href = "alerta.php?hora=" +compromissos[i].hora + "&assunto=" + compromissos[i].assunto;
+                }
+            }
+        }
+
+        setInterval(atualizarRelogio, 1000);    // Atualiza o relógio a cada 1s
+
+        // Cria objeto compromisso
+        function compromisso(h, a) {
+            this.hora = h;
+            this.assunto = a;
+        }
+
+        // Função que inclui o compromisso no vetor
+        function incluirComp () { 
+            // Adiciona compromisso
+            var hora = document.getElementById("hora").value;
+            var assunto = document.getElementById("assunto").value;
+
+            compromissos.push( new compromisso (hora, assunto) );
+            alert("Compromisso salvo: " + assunto + " às " + hora);
+
+        }
+    </script>
+</body>
+</html>
---
+++ INFO5/web/php/ativAlarme/alerta.php
@@ -0,0 +1,52 @@
+<?php
+
+    $hora = $_GET["hora"];
+    $assunto = $_GET["assunto"];
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+    
+<head>
+    <meta charset="UTF-8">
+    <title>ALERTA</title>
+    <style>
+        body{
+            background-color: black;
+            color: green;
+            text-align: center;
+            margin-top: 200px;
+            font-size: 40px;
+            font-family: Arial;
+        }
+        .pisca{
+            animation: piscar 2s infinite;
+        }
+        @keyframes piscar {
+
+            0% {
+                opacity: 1;
+            }
+
+            50% {
+                opacity: 0;
+            }
+
+            100% {
+                opacity: 1;
+            }
+        }
+    </style>
+</head>
+
+<body>
+<div class="pisca">
+    COMPROMISSO AGORA!<br><br>
+    <?php
+        echo $hora . "<br><br>";
+        echo $assunto;
+    ?>
+</div>
+</body>
+
+</html>
